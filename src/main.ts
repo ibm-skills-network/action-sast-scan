8f078e65b583f0c26a89f4b1c8f09fd8e30f345a
Allan <62227321+allgeo@users.noreply.github.com>
2024-04-04 14:49:15 -0400

    test

--- src/main.ts
+++ src/main.ts
@@ -35,6 +35,8 @@ export async function runSastScan(): Promise<void> {
       `wget -O scanner.jar --header="X-JFrog-Art-Api: ${jfrogToken}" https://na.artifactory.swg-devops.com/artifactory/css-whitesource-team-java-contrast-agent-maven-local/sast-local-scan-runner-${contrastAgentVersion}.jar`
     )
 
+    console.log(`CONTRAST__API__URL: ${process.env.CONTRAST__API__URL}`);  
+
     //Run the SAST scan
     core.info('Running SAST scan...')
     const scanCommand = `java -jar scanner.jar ${fileToBeScanned} --project-name ${projectName} --label ${userName} -r "${resourceGroup}"`
